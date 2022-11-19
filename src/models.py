515f033f5098842d6e1e84b8fbab9e2a2811dc6a
JuanjoRuiz <juanjoserusa@gmail.com>
2022-11-19 10:43:22 +0000

    instagram diagram

--- src/models.py
+++ src/models.py
@@ -1,12 +1,14 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -18,38 +20,76 @@ class User(Base):
     def to_dict(self):
         return {}
 
-class Character(Base):
-    __tablename__ = 'character'
-    id = Column(String(250), primary_key=True)
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Enum, primary_key=True)
     name = Column(String(250))
-    hair_color = Column(String(250))
+    url = Column(String(250))
+    type_media = Column(String(30), ForeignKey('post.id'))
     
     
     def to_dict(self):
         return {}
 
-class Planet(Base):
-    __tablename__ = 'planet'
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(String(250), primary_key=True)
-    name = Column(String(250))
-    diameter = Column(Integer)
-    population = Column(Integer)
-    climate = Column(String(250))
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
 
     
     def to_dict(self):
         return {}
 
-class Favorite(Base):
-    __tablename__ = 'favorite'
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(String(250), primary_key=True)
+    media_id = Column(Enum, ForeignKey('media.id'))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+    
+
+    
+    def to_dict(self):
+        return {}
+
+class Report(Base):
+    __tablename__ = 'report'
+    id = Column(String(250), primary_key=True)
+    reported_text = Column(String(250), ForeignKey('comment.comment_text'))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
+
+    
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    character_id = Column(String(250), ForeignKey('character.id'))
-    planet_id = Column((String(250)), ForeignKey('planet.id'))
+    media_id = Column(Enum, ForeignKey('media.id'))
+    
+
+
+    def to_dict(self):
+        return {}
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(String(250), primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    
 
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+render_er(Base, 'diagram_instagram.png')
